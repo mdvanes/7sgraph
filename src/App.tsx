7fe4678d32971fa5ab2f167f547b7e6cdb72766c
M.D. van Es <es@mdworld.nl>
2020-12-19 10:12:52 +0100

    Set up GraphiQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GraphiQL from "graphiql";
+import logo from "./armillarysphere.svg";
+import { FetcherParams } from "graphiql/dist/components/GraphiQL";
+import "./App.css";
+import "graphiql/graphiql.css";
+
+const hackyTest = async () => {
+  const response = (
+    await fetch("http://localhost:8080/query", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+      credentials: "omit",
+      body: JSON.stringify({
+        query: `me(func: has(starring)) {
+        name
+       }
+     `,
+        variables: {},
+      }),
+    })
+  ).json();
+  // const response = (
+  //   await fetch("http://localhost:8080/query", {
+  //     method: "post",
+  //     headers: { "Content-Type": "application/json" },
+  //     mode: "cors",
+  //     credentials: "omit",
+  //     body: JSON.stringify({
+  //       query: `me(func: has(starring)) {
+  //       name
+  //      }
+  //    `,
+  //       variables: {},
+  //     }),
+  //   })
+  // ).json();
+  console.log(response);
+};
+
+const graphQLFetcher = async (graphQLParams: FetcherParams) => {
+  return (
+    await fetch("http://localhost:8080/graphql", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+      credentials: "omit",
+      body: JSON.stringify(graphQLParams),
+    })
+  ).json();
+};
 
 function App() {
+  // hackyTest();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>test query:</p>
+      <pre>
+        {`{
+ me(func: has(starring)) {
+   name
+  }
+}`}
+      </pre>
+      <GraphiQL fetcher={graphQLFetcher} />
     </div>
   );
 }
