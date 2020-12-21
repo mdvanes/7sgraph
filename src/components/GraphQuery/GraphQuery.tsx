ea5cfe983397d7f118ef6cc88b3a7353ad385e8b
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-12-21 12:26:53 +0100

    Filter links to existing nodes

--- src/components/GraphQuery/GraphQuery.tsx
+++ src/components/GraphQuery/GraphQuery.tsx
@@ -1,5 +1,9 @@
 import { Graph } from "react-d3-graph";
-import { Maybe, useGetAllPersonsQuery, useGetPersonByNameQuery } from "../../generated/graphql";
+import {
+  Maybe,
+  useGetAllPersonsQuery,
+  useGetPersonByNameQuery,
+} from "../../generated/graphql";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import appGraphConfig from "./appGraphConfig";
 
@@ -34,40 +38,59 @@ function isJustVal<T>(val: Maybe<T>): val is T {
 // };
 
 const GraphQuery = () => {
-  const { data /*, loading, error */ } = useGetAllPersonsQuery({
+  const { data /* loading,*/, error } = useGetAllPersonsQuery({
     client,
   });
-  // const { data /*, loading, error */ } = useGetPersonByNameQuery({
-  //   client, variables: { name: "Salt"}
+  // const { data /*, loading */, error } = useGetPersonByNameQuery({
+  //   client,
+  //   variables: { name: "Salt" },
   // });
+
+  if (error) {
+    console.error(error);
+  }
 
   const justPersons = data?.queryPerson?.filter(isJustVal) ?? [];
 
+  const nodes = justPersons.map(({ personID, name }) => ({
+    id: personID || "a",
+    name: name || "",
+  }));
+
+  const links = justPersons.flatMap(
+    ({ personID, parent, nonBioParent, physicalRelation }) => {
+      const relatedParent =
+        parent?.filter(isJustVal).map((parentPerson) => ({
+          source: personID,
+          target: parentPerson?.personID ?? "",
+        })) ?? [];
+      const relatedNonBio =
+        nonBioParent?.filter(isJustVal).map((nonBioParentPerson) => ({
+          source: personID,
+          target: nonBioParentPerson?.personID ?? "",
+        })) ?? [];
+      const relatedPhys =
+        physicalRelation?.filter(isJustVal).map((physicalRelationPerson) => ({
+          source: personID,
+          target: physicalRelationPerson?.personID ?? "",
+        })) ?? [];
+      return relatedParent.concat(relatedNonBio, relatedPhys);
+    }
+  );
+
+  const linksToExistingNodes = links.filter(
+    (link) =>
+      nodes.some((n) => n.id === link.source) &&
+      nodes.some((n) => n.id === link.target)
+  );
+
   const graphData = {
-    nodes: justPersons.map(({ personID, name }) => ({
-      id: personID || "a",
-      name: name || "",
-    })),
-    links: justPersons.flatMap(
-      ({ personID, parent, nonBioParent, physicalRelation }) => {
-        const relatedParent =
-          parent?.filter(isJustVal).map((parentPerson) => ({
-            source: personID,
-            target: parentPerson?.personID ?? "",
-          })) ?? [];
-        const relatedNonBio =
-          nonBioParent?.filter(isJustVal).map((nonBioParentPerson) => ({
-            source: personID,
-            target: nonBioParentPerson?.personID ?? "",
-          })) ?? [];
-        const relatedPhys =
-          physicalRelation?.filter(isJustVal).map((physicalRelationPerson) => ({
-            source: personID,
-            target: physicalRelationPerson?.personID ?? "",
-          })) ?? [];
-        return relatedParent.concat(relatedNonBio, relatedPhys);
-      }
-    ),
+    nodes,
+    links: linksToExistingNodes,
+  };
+
+  const onClickNode = (uid: string) => {
+    console.log(uid);
   };
 
   return (
@@ -76,8 +99,11 @@ const GraphQuery = () => {
         <Graph
           id="graph-id" // id is mandatory
           data={graphData}
-          config={appGraphConfig(window.innerWidth, window.innerHeight - 64 - 5)}
-          //   onClickNode={onClickNode}
+          config={appGraphConfig(
+            window.innerWidth,
+            window.innerHeight - 64 - 5
+          )}
+          onClickNode={onClickNode}
           //   onClickLink={onClickLink}
         />
       )}
