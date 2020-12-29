edb78b964ab4962d46dbdf5f78d44c31c5f3023f
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-12-29 09:10:28 +0100

    Generate populate.sh

--- populate.js
+++ populate.js
@@ -2,10 +2,30 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const results = [];
 
+const OUTPUT_FILE_NAME = "populate.sh";
+
 // Bash escape single quote
 const escape = (str) => str.replace(/'/g, "'\\''");
 
-const escapeMutationStr = (str) => str.replace(/"/g, "\\\"").replace(/\n|\s{2}/g, " ");
+const escapeMutationStr = (str) =>
+  str.replace(/"/g, '\\"').replace(/\n|\s{2}/g, " ");
+
+const createNickNamesField = (v) =>
+  v &&
+  `, nickNames: [${v
+    .split(",")
+    .map((n) => `"${n}"`)
+    .join(", ")}]`;
+
+const createStrField = (name, v) => v && `, ${name}: "${v}"`;
+const createField = (name, v) => v && `, ${name}: ${v}`;
+
+const createRelationField = (name, v) =>
+  v &&
+  `, ${name}: [${v
+    .split(",")
+    .map((n) => `{ personID: "${n}" }`)
+    .join(", ")}]`;
 
 fs.createReadStream("persons.csv")
   .pipe(csv())
@@ -19,32 +39,46 @@ fs.createReadStream("persons.csv")
 addPerson(input: [
     { personID:"${person.personID}"
     , name: "${escape(person.name)}"
-    , nickNames: [${person.nickNames
-      .split(",")
-      .map((n) => `"${n}"`)
-      .join(", ")}]
-    , dateOfBirth: ${person.dateOfBirth || 0}
-    , children: [${person.children
-      .split(",")
-      .map((n) => `{ personID: "${n}" }`)
-      .join(", ")}]
+    ${createNickNamesField(person.nickNames)}
+    ${createStrField("gender", person.gender)}
+    ${createField("dateOfBirth", person.dateOfBirth)}
+    ${createField("dateOfDeath", person.dateOfDeath)}
+    ${createRelationField("children", person.children)}
+    ${createRelationField("nonBioChildren", person.nonBioChildren)}
+    ${createRelationField("parents", person.parents)}
+    ${createRelationField("nonBioParents", person.nonBioParents)}
+    ${createRelationField("physicalRelation", person.physicalRelation)}
+    ${createRelationField("otherRelation", person.otherRelation)}
 }]) {
     person {
     personID
     name
     nickNames
+    gender
     dateOfBirth
+    dateOfDeath
     children {
         personID
     }
     nonBioChildren {
         personID
     }
+    parents {
+        personID
+    }
+    nonBioParents {
+        personID
+    }
+    physicalRelation {
+        personID
+    }
+    otherRelation {
+        personID
+    }
     }
 }
 }`;
       console.log(mutationStr);
-      //   return mutationStr;
       return `curl 'http://localhost:8080/graphql' -H 'Content-Type: application/json' --compressed --data-binary '{"query":"${escapeMutationStr(
         mutationStr
       )}","variables":null}'`;
@@ -52,30 +86,18 @@ addPerson(input: [
     // console.log(curlStrings);
 
     try {
-      fs.writeFileSync("populate2.sh", "");
-      console.log("emptied populate2.sh");
+      fs.writeFileSync(OUTPUT_FILE_NAME, "");
+      console.log(`emptied ${OUTPUT_FILE_NAME}`);
     } catch (err) {
       console.log("error emptying: " + err);
     }
 
-    const stream = fs.createWriteStream("populate2.sh", { flags: "a" });
+    const stream = fs.createWriteStream(OUTPUT_FILE_NAME, { flags: "a" });
     curlStrings.forEach((str) => {
       stream.write(str + "\n\n\n");
     });
-    console.log("wrote populate2.sh");
+    stream.write("echo ;");
+    stream.write("echo 🎉 Finished curl scripts with graphql mutate statements");
     stream.end();
+    console.log(`Wrote ${OUTPUT_FILE_NAME}`);
   });
-
-//   TODO add escaping for e.g. name
-
-//   _:maia <Person.name> "Maia d\'Apliése" .
-//   _:maia <Person.personID> "maia" .
-//   _:maia <Person.gender> "female" .
-//   _:maia <dgraph.type> "Person" .
-//   _:maia <Person.story> _:book1 .
-//   _:maia <Person.physicalRelation> _:zed .
-
-//   curl 'http://localhost:8080/graphql' \
-//   -H 'Content-Type: application/json' \
-//   --data-binary '{"query":"mutation {\n  addPerson(input: [\n    { personID:\"thomfhalvorsen\", name: \"Thom Felix Halvorsen\", dateOfBirth: 1980 },\n  ]) {\n    person {\n      personID\n      name\n      children {\n        personID\n      }\n    }\n  }\n}","variables":null}' \
-//   --compressed
