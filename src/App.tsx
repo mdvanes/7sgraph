707c127c1d2639ad5db7a45096f8e10beb310a35
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-12-29 16:39:23 +0100

    Add spoiler alert

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import "./App.css";
 import "graphiql/graphiql.css";
 import AppBar from "@material-ui/core/AppBar/AppBar";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GraphDebugger from "./components/GraphDebugger/GraphDebugger";
 import GraphQuery from "./components/GraphQuery/GraphQuery";
 import AppToolbar from "./components/AppToolbar/AppToolbar";
+import StartDialog from "./components/StartDialog/StartDialog";
 
 function App() {
   return (
     <Router>
+      <StartDialog />
       <div className="App">
         <AppBar position="static">
           <AppToolbar />
---
+++ src/components/GraphQuery/GraphTools.tsx
@@ -0,0 +1,10 @@
+const GraphTools = () => {
+  // TODO add dropdown
+  // TODO wrap in Card and overlay over graph
+  // TODO add "details" view Card in corner
+  return (
+    <div style={{ position: "absolute" }}>Search by book: TODO dropdown</div>
+  );
+};
+
+export default GraphTools;
