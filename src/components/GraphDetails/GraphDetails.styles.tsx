2cf7239d0b91d5567dfcb35c46fb19fc513c3549
M.D. van Es <es@mdworld.nl>
2021-01-01 12:21:40 +0100

    Add GraphDetails

---
+++ src/components/GraphDetails/GraphDetails.styles.tsx
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(({ spacing }) => ({
+  root: {
+      position: "absolute",
+      bottom: spacing(2),
+      right: spacing(2),
+  },
+  cardRoot: {
+    backgroundColor: "rgba(255,255,255,0.5)",
+    height: 225,
+    width: 200,
+  },
+}));
+
+export default useStyles;
