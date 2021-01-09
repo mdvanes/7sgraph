a5c08c6e805e40b350894f56d9aa7a8b3282e477
M.D. van Es <es@mdworld.nl>
2021-01-09 16:21:43 +0100

    Hide controls on mobile

--- src/components/GraphTools/GraphTools.styles.ts
+++ src/components/GraphTools/GraphTools.styles.ts
@@ -5,6 +5,13 @@ const useStyles = makeStyles((theme) => ({
     position: "absolute",
     width: `calc(100% - ${theme.spacing(4)}px)`,
     margin: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+    transition: "height 200ms"
+  },
+  closed: {
+    overflow: "hidden",
+    height: theme.spacing(2),
+    width: theme.spacing(2),
+    transition: "height 200ms"
   },
   gridRoot: {
     flexGrow: 1,
@@ -19,6 +26,11 @@ const useStyles = makeStyles((theme) => ({
   },
   formControlRoot: {
     width: "100%",
+  },
+  collapseButton: {
+    position: "absolute",
+    left: theme.spacing(-2),
+    top: theme.spacing(-2),
   },
 }));
 
