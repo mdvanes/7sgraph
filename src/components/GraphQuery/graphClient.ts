c7a5e8333ca42faea6d79695cafe6b0e75386b30
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-02-11 20:34:33 +0100

    Add lite mode

--- src/components/GraphQuery/graphClient.ts
+++ src/components/GraphQuery/graphClient.ts
@@ -1,7 +1,11 @@
 import { GraphQLClient } from "graphql-request";
 
-const client = new GraphQLClient(process.env["REACT_APP_GRAPHQL_URL"] || "", {
+const url = process.env["REACT_APP_GRAPHQL_URL"] || "";
+
+const client = new GraphQLClient(url, {
   headers: {},
 });
+
+export const isLiteMode = url.indexOf(".json") > 0;
 
 export default client;
